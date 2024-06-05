eedc8d99d53ab941d738fc9e268f5c9e756c9a3f
JayanDesilva <jaycdesilva@gmail.com>
2024-06-05 14:01:04 -0400

    Working with sets

--- src/main/java/com/amigoscode/WorkingWithSets.java
+++ src/main/java/com/amigoscode/WorkingWithSets.java
@@ -1,2 +1,21 @@
-package com.amigoscode;public class WorkingWithSets {
+package com.amigoscode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+record Ball(String color) {}
+
+public class WorkingWithSets {
+    public static void main(String[] args) {
+
+        Set<Ball> balls = new HashSet<>();
+        balls.add(new Ball("Yellow"));
+        balls.add(new Ball("Green"));
+        balls.add(new Ball("Red"));
+        balls.add(new Ball("Blue"));
+        balls.add(new Ball("Orange"));
+
+        balls.forEach(System.out::println);
+
+    }
 }
