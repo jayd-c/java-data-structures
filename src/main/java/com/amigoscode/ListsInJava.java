a5316d288bf1949c739bd599ad33186416953c0d
JayanDesilva <jaycdesilva@gmail.com>
2024-06-05 12:19:48 -0400

    Lists

---
+++ src/main/java/com/amigoscode/ListsInJava.java
@@ -0,0 +1,21 @@
+package com.amigoscode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListsInJava {
+    public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Red");
+        stringList.add("Yellow");
+        stringList.add("Green");
+
+        System.out.println(stringList);
+        stringList.forEach(String::toLowerCase);
+        System.out.println(stringList);
+
+
+    }
+}
